4addf02b22372551d5f9a266c29833403aa6c1cd
Getty Ritter <gettylefou@gmail.com>
2016-01-31 11:21:08 -0800

    Added file locking to filesystem backend

--- Database/Tansu/Backend/Filesystem.hs
+++ Database/Tansu/Backend/Filesystem.hs
@@ -5,6 +5,7 @@ import Data.ByteString.Base64
 import qualified Data.ByteString.Char8 as BS
 import Database.Tansu.Internal (Database(..))
 import System.Directory (createDirectoryIfMissing, doesFileExist)
+import System.FileLock (SharedExclusive(Exclusive), withFileLock)
 import System.FilePath.Posix ((</>))
 
 filePathSet :: FilePath -> ByteString -> ByteString -> IO ()
@@ -20,10 +21,14 @@ filePathGet path key = do
     then Just `fmap` BS.readFile keyPath
     else return Nothing
 
+filePathLock :: FilePath -> IO a -> IO a
+filePathLock path comp = do
+  withFileLock (path </> ".lock") Exclusive (const comp)
+
 withFilesystemDb :: FilePath -> (Database -> IO a) -> IO a
 withFilesystemDb path comp = do
   createDirectoryIfMissing True path
-  comp $ Database { dbSet = filePathSet path
-                  , dbGet = filePathGet path
-                  , dbRunTransaction = id
+  comp $ Database { dbSet            = filePathSet path
+                  , dbGet            = filePathGet path
+                  , dbRunTransaction = filePathLock path
                   }
